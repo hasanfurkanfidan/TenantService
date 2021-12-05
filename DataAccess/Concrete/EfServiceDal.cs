f80018b4c217111dc98aead2c68ea80ce56ba6a5
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-12-05 11:07:11 +0300

    All te data  access repositories completed successfully

---
+++ DataAccess/Concrete/EfServiceDal.cs
@@ -0,0 +1,13 @@
+﻿using Core.DataAccess.Concrete;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete
+{
+    public class EfServiceDal : EfEntityRepositoryBase<Service>
+    {
+        public EfServiceDal(TenantServiceContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/EfTenantDal.cs
@@ -0,0 +1,13 @@
+﻿using Core.DataAccess.Concrete;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete
+{
+    public class EfTenantDal : EfEntityRepositoryBase<Tenant>
+    {
+        public EfTenantDal(TenantServiceContext context) : base(context)
+        {
+
+        }
+    }
+}
